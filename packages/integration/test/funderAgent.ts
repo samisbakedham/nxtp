9f8fe14b95e67de91d614287d3d8e3ec8dae9758
@base0010 <marcus@gatekeeper.network>
2021-12-23 08:18:25 -0700

    adding funderAgent logic

--- packages/integration/test/funderAgent.ts
+++ packages/integration/test/funderAgent.ts
@@ -1,6 +1,7 @@
 import { getOnchainBalance, jsonifyError } from "@connext/nxtp-utils";
 import { constants, providers, Signer, utils, Wallet } from "ethers";
 import { Logger } from "pino";
+import { stringify } from "querystring";
 import { getDecimals } from "./utils/chain";
 
 class AccountManager{
@@ -9,31 +10,51 @@ class AccountManager{
   
   constructor(private readonly signer: Wallet,
               private readonly log:Logger,
-              private readonly USER_MIN_TOKEN:number,
-              private readonly USER_MIN_ETH:number, ){
+              private readonly USER_MIN_TOKEN:string = "5",
+              private readonly USER_MIN_ETH:string = "0.2",
+              private readonly TOKEN_MULTIPLE:string = "1",
+              private readonly ETH_MULTIPLE:string = "1"){
     
   }
-  async updateBalances(chainId: number, agentAddress:string, assetId: string = constants.AddressZero){
+  async getAssetDecmials(assetId:string){
+    const decimals = this.cachedDecimals[assetId] ? this.cachedDecimals[assetId] : await getDecimals(assetId, this.signer);
+    this.cachedDecimals[assetId] = decimals;
+    return decimals;
+  }
+
+  async updateDecimals(chainId: number, agentAddress:string, assetId: string = constants.AddressZero){
     try {
-      const decimals = this.cachedDecimals[assetId] ? this.cachedDecimals[assetId] : await getDecimals(assetId, this.signer);
-      this.cachedDecimals[assetId] = decimals;
+      const decimals = await this.getAssetDecmials(assetId);
+      this.log.debug(`Got asset decimals...`);
+      return decimals;
     } catch (e) {
       this.log.error("Failed to get decimals!", undefined, undefined, jsonifyError(e), { chainId, assetId });
     }
+
+  }
+  async verifyAndUpdateBalance(funderWallet:Wallet, agentAddress:string, chainId:number, assetId:string){
     
+    const providerNetwork = await funderWallet.provider.getNetwork();
+
+    if(providerNetwork.chainId !== chainId){
+      this.log.error(`Provider not configured for ${chainId}`);
+      throw new Error(`Incorrect Provider`);
+    }
+    const decimals = await this.updateDecimals(chainId, agentAddress, assetId);
     const isToken = assetId !== constants.AddressZero;
-    const floor = isToken ? utils.parseUnits(this.USER_MIN_TOKEN, decimals) : this.USER_MIN_ETH;
-
-    const initialAmount = await getOnchainBalance(assetId, agentAddress, this.signer.provider );
-
-    if (initialAmount.gte(floor)) {
+    const floor = isToken ? utils.parseUnits(this.USER_MIN_TOKEN, decimals) : utils.parseEther(this.USER_MIN_ETH);
+    const initial = await getOnchainBalance(assetId, agentAddress, funderWallet.provider);
+    
+    if (initial.gte(floor)) {
       this.log.info("No need for top up", undefined, undefined, { assetId, agentAddress, chainId });
       return initial;
     }
 
+    const toSend = isToken
+      ? floor.mul(this.TOKEN_MULTIPLE)
+      : floor.sub(initial).mul(this.ETH_MULTIPLE);
 
   }
-
 
 }
 
--- packages/integration/test/utils/config.ts
+++ packages/integration/test/utils/config.ts
@@ -97,3 +97,4 @@ export const getConfig = (useDefaultLocal = false): Config => {
     chainConfig,
   };
 };
+
